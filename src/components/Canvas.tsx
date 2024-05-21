b595791392ba3f5900090aea6f98b613ba8abf4d
junheeLee96 <dlwnsgml203@naver.com>
2024-05-21 18:31:34 +0900

    zoom and moving

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef } from "react";
 const maxZoom = 5;
 const minZoom = 0.01;
 const scrollSensitivity = 5;
+const zoomSensitivity = 0.001;
 const Canvas = ({ data, imgsData }: any) => {
   const ref = useRef<null | HTMLCanvasElement>(null);
   const ctxRef = useRef<null | CanvasRenderingContext2D>(null);
@@ -13,7 +14,7 @@ const Canvas = ({ data, imgsData }: any) => {
   const cameraZoomRef = useRef<number>(0.1);
   //   const scroll_sensitivityRef = useRef<number>(0.0005);
   const isDraggingRef = useRef<boolean>(false);
-  const lastZoomRef = useRef<number | null>(cameraZoomRef.current);
+  const lastZoomRef = useRef<number>(cameraZoomRef.current);
   const dragStartRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
   //   const initialPinchDistanceRef = useRef(null);
   //   const childrenRef = useRef(null);
@@ -29,10 +30,6 @@ const Canvas = ({ data, imgsData }: any) => {
   const drawFigure = (child: any) => {
     if (!ctxRef.current) return;
     ctxRef.current.beginPath();
-
-    // if (child.name === "Wifi") {
-    //   console.log(child);
-    // }
 
     const { type } = child;
     switch (type) {
@@ -68,38 +65,6 @@ const Canvas = ({ data, imgsData }: any) => {
       child.children.forEach(drawFigure);
     }
   };
-  const getEventLocation = (e: any): any => {
-    if (e.touches && e.touches.length == 1) {
-      return { x: e.touches[0].clientX, y: e.touches[0].clientY };
-    } else if (e.clientX && e.clientY) {
-      return { x: e.clientX, y: e.clientY };
-    }
-  };
-  const onScreenMove = (e: any) => {
-    e.preventDefault();
-    cameraOffsetRef.current = {
-      x: cameraOffsetRef.current.x - e.deltaX * scrollSensitivity,
-      y: cameraOffsetRef.current.y - e.deltaY * scrollSensitivity,
-    };
-
-    console.log(cameraOffsetRef.current.x, cameraOffsetRef.current.y);
-    // const x: any =
-    //   getEventLocation(e).x / cameraZoomRef.current - cameraOffsetRef.current.x;
-    // const y: any =
-    //   getEventLocation(e).y / cameraZoomRef.current - cameraOffsetRef.current.y;
-    // dragStartRef.current = {
-    //   x,
-    //   y,
-    // };
-    // console.log(x, y);
-    // cameraOffsetRef.current.x =
-    //   getEventLocation(e).x / cameraZoomRef.current - x;
-    // cameraOffsetRef.current.y =
-    //   getEventLocation(e).y / cameraZoomRef.current - y;
-    // lastZoomRef.current = cameraZoomRef.current;
-    // console.log(e);
-    // drawStart();
-  };
 
   const drawStart = () => {
     if (!ref.current || !ctxRef.current) return;
@@ -116,35 +81,36 @@ const Canvas = ({ data, imgsData }: any) => {
     ctxRef.current.clearRect(0, 0, window.innerWidth, window.innerHeight);
     const arr = data.document;
     arr.children.forEach(drawFigure);
-    requestAnimationFrame(drawStart);
+    // requestAnimationFrame(drawStart);
   };
-  const onPointerDown = (e: any) => {
-    isDraggingRef.current = true;
-    const x =
-      getEventLocation(e).x / cameraZoomRef.current - cameraOffsetRef.current.x;
-    const y =
-      getEventLocation(e).y / cameraZoomRef.current - cameraOffsetRef.current.y;
-    dragStartRef.current = {
-      x,
-      y,
+  const onScreenMove = (e: any) => {
+    e.preventDefault();
+    if (e.ctrlKey) {
+      adjustZoom(-e.deltaY * zoomSensitivity, null);
+    }
+    cameraOffsetRef.current = {
+      x: cameraOffsetRef.current.x - e.deltaX * scrollSensitivity,
+      y: cameraOffsetRef.current.y - e.deltaY * scrollSensitivity,
     };
+
+    console.log(cameraOffsetRef.current.x, cameraOffsetRef.current.y);
+
+    drawStart();
   };
 
-  const onPointerMove = (e: any) => {
-    if (isDraggingRef.current) {
-      cameraOffsetRef.current.x =
-        getEventLocation(e).x / cameraZoomRef.current - dragStartRef.current.x;
-      cameraOffsetRef.current.y =
-        getEventLocation(e).y / cameraZoomRef.current - dragStartRef.current.y;
+  const adjustZoom = (zoomAmount: any, zoomFactor: any) => {
+    if (!isDraggingRef.current) {
+      if (zoomAmount) {
+        cameraZoomRef.current += zoomAmount;
+      } else if (zoomFactor) {
+        console.log(zoomFactor);
+        cameraZoomRef.current = zoomFactor * lastZoomRef.current;
+      }
+
+      cameraZoomRef.current = Math.min(cameraZoomRef.current, maxZoom);
+      cameraZoomRef.current = Math.max(cameraZoomRef.current, minZoom);
     }
   };
-
-  const onPointerUp = () => {
-    isDraggingRef.current = false;
-    // initialPinchDistanceRef.current = null;
-    lastZoomRef.current = cameraZoomRef.current;
-  };
-
   useEffect(() => {
     if (!ref.current) return;
     const ctx = ref.current.getContext("2d");
@@ -154,9 +120,7 @@ const Canvas = ({ data, imgsData }: any) => {
     ctx.fillStyle = "red";
     ctx.fillRect(-500, -500, 500, 500);
     drawStart();
-    ref.current.addEventListener("mousedown", onPointerDown);
-    ref.current.addEventListener("mousemove", onPointerMove);
-    ref.current.addEventListener("mouseup", onPointerUp);
+
     ref.current.addEventListener("wheel", onScreenMove);
   }, []);
 
